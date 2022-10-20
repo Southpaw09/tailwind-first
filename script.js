c7c9706f4bfb18d1d39d124b138307456d439e11
Ayush <k.mishra.ayush98@gmail.com>
2022-10-20 14:42:22 +0530

    Responsive Navbar

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const menuIcon = document.getElementById("menu");
+const hamburger = document.getElementById("hamburger");
 const detailedMenu = document.getElementById("detailedMenu");
+const remove = document.getElementById("remove");
 
-menuIcon.addEventListener("click", () => {
-  detailedMenu.classList.toggle("hidden");
+hamburger.addEventListener("click", () => {
+  detailedMenu.classList.remove("left-[-500vh]");
+  detailedMenu.classList.remove("top-[-500vh]");
+  detailedMenu.classList.add("top-0.5");
+  detailedMenu.classList.add("left-0.5");
+});
+
+remove.addEventListener("click", () => {
+  detailedMenu.classList.remove("top-0.5");
+  detailedMenu.classList.remove("left-0.5");
+  detailedMenu.classList.add("left-[-500vh]");
+  detailedMenu.classList.add("top-[-500vh]");
 });
